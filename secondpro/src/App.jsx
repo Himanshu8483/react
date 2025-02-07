241b9ab745d4fe0c0809b00178bfaa75a14010a7
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-02-07 20:32:52 +0530

    all

--- secondpro/src/App.jsx
+++ secondpro/src/App.jsx
@@ -1,20 +1,35 @@
-import Navbar from "./navbar"
-import "./App.css"
+import Navbar from "./navbar"; // Importing the Navbar component
+import "./App.css"; // Importing external CSS file
+
 function App() {
+  // Defining a style object for reuse
   let para = {
-    color:"blue",
-    backgroundColor:"green"
-  }
+    color: "blue", 
+    backgroundColor: "green" 
+  };
+
   return (
     <>
-    <Navbar/>
-    <h1 style={{color:"red"}}>Welcome</h1>
-    <p style={{backgroundColor:"green", color:"red"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-    <p style={{...para, backgroundColor:"skyblue",  height:"300px"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis consectetur minus repellat delectus dolorem possimus deleniti, debitis voluptatem magni molestiae nam maxime explicabo voluptates id fuga qui, ipsa at quia?</p>
-    <p style={para}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis consectetur minus repellat delectus dolorem possimus deleniti, debitis voluptatem magni molestiae nam maxime explicabo voluptates id fuga qui, ipsa at quia?</p>
-    
-    </>
-  )
-}
+      {/* Rendering the Navbar component */}
+      <Navbar />
 
-export default App
+      {/* Inline CSS example - Text color is set directly */}
+      <h1 style={{ color: "red" }}>Welcome</h1>
+
+      {/* Another inline CSS example - Setting background color and text color */}
+      <p style={{ backgroundColor: "green", color: "red" }}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+
+      {/* Using both object styles + inline styles together */}
+      {/* The spread operator (...para) merges the existing object styles */}
+      {/* The backgroundColor is overwritten by "skyblue" */}
+      <p style={{ ...para, backgroundColor: "skyblue", height: "300px" }}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+
+      {/* Using only the predefined object styles */}
+      <p style={para}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+    </>
+  );
+}
+export default App;
