954b8fd708c4e522f6cc7d240c91fdd8cb069ec5
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-03-07 12:48:24 +0530

    all

--- api/src/App.jsx
+++ api/src/App.jsx
@@ -1,15 +1,42 @@
 import axios from 'axios'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
+  const [comment, setComment]=useState([])
+  const tableData = comment.map((e)=>(
+    <tr key={e.id}>
+    <td>{e.name}</td>
+    <td>{e.age}</td>
+    <td>{e.contact}</td>
+    <td>{e.city}</td>
+    <td>{e.id}</td>
+  </tr>
+  ))
   useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/comments')
-    .then(res=>{console.log(res.data)})
+    // axios.get('https://jsonplaceholder.typicode.com/comments')
+    axios.get('http://localhost:3000/students')
+    .then(res=>{console.log(res.data), setComment(res.data)})
   },[])
   return(
     <>
-    <h1>Hello</h1>
+    <h1>Dashboard</h1>
+    <div>
+      <table border="2"  cellPadding={50}>
+        <thead>
+          <tr style={{background:"aquamarine"}}>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Contact</th>
+            <th>City</th>
+            <th>Id</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData}
+        </tbody>
+      </table>
+    </div>
     </>
   )
 }
